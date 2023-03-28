1c59b7af169c5388b7e5e96ab6343929c3a3b647
Morgan Jackson <morjack@siue.edu>
2023-03-28 02:31:43 -0500

    implemented functional weather + forecast widgets

--- scripts/weather.py
+++ scripts/weather.py
@@ -4,6 +4,7 @@ import sys
 import os
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 fontdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic/fonts')
+weatherdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic/weather')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 widgetdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'widgets')
 if os.path.exists(libdir):
@@ -13,22 +14,103 @@ import logging
 from waveshare_epd import epd7in5b_V2
 import time
 from PIL import Image,ImageDraw,ImageFont
+import PIL.ImageOps
 import traceback
 from datetime import datetime
 import requests
-from dotenv import dotenv_values
-
-config = dotenv_values(".env")
+import json
 
 logging.basicConfig(level=logging.DEBUG)
 
-def get_weather_widget(width: int, height: int, x: int, y: int, red_Channel: ImageDraw, black_Channel: ImageDraw, font24, font18):
+def temp_string(temp: float, units: str):
+    if units == "imperial":
+        return f"{temp}°F"
+    else:
+        return f"{temp}°C"
+
+def get_weather_widget(width: int, height: int, x: int, y: int, red_Channel: ImageDraw, black_Channel: ImageDraw, font48):
     try:   
-        apiURL = f"https://api.openweathermap.org/data/2.5/weather?lat={config.lat}&lon={config.long}&appid={config.weatherKey}&units={config.units}"
+        # load json file into a dictionary
+        with open('config.json') as json_file:
+            config = json.load(json_file)        
+
+        apiURL = f"https://api.openweathermap.org/data/2.5/weather?lat={config['lat']}&lon={config['long']}&appid={config['weatherKey']}&units={config['units']}"
+        response = requests.get(apiURL)
+        data = response.json()
+        
+        xOffset = (width - 100) // 2
+        
+        if height >= 140:
+            yOffset = (height - 140) // 2
+        else:
+            yOffset = (height - 40) // 2
+
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
+        rawTempStr = str(temp)
+        tempStr = f"{rawTempStr.split('.')[0]}°"
+        tempLen = font48.getlength(tempStr)
+        tempX = x + xOffset + (width - tempLen) // 2
+        black_Channel.text((tempX, yOffset + y), tempStr, font = font48, fill = 0)
+        if height >= 140:
+            if os.path.isfile(os.path.join(weatherdir, icon + ".bmp")):
+                iconImg = Image.open(os.path.join(weatherdir, icon + ".bmp")).convert('L')
+                iconImg = PIL.ImageOps.invert(iconImg)
+                black_Channel.bitmap((xOffset + x, yOffset + y + 40), iconImg, fill = 0)
+            if os.path.isfile(os.path.join(weatherdir, icon + "r.bmp")):
+                iconImg = Image.open(os.path.join(weatherdir, icon + "r.bmp")).convert('L')
+                iconImg = PIL.ImageOps.invert(iconImg)
+                red_Channel.bitmap((xOffset + x, yOffset + y + 40), iconImg, fill = 0)
+          
+    except IOError as e:
+        logging.info(e)
+        
+    except KeyboardInterrupt:    
+        logging.info("ctrl + c:")
+        epd7in5b_V2.epdconfig.module_exit()
+        exit()
+
+def get_forecast_widget(width: int, height: int, x: int, y: int, red_Channel: ImageDraw, black_Channel: ImageDraw, font24):
+    try:   
+        # load json file into a dictionary
+        with open('config.json') as json_file:
+            config = json.load(json_file)        
+
+        apiURL = f"https://api.openweathermap.org/data/2.5/forecast?lat={config['lat']}&lon={config['long']}&appid={config['weatherKey']}&units={config['units']}"
         response = requests.get(apiURL)
         data = response.json()
 
-    
+        xOffset = (width % 100) / 2
+        if height >= 140:
+            yOffset = (height - 140) / 2
+        else:
+            yOffset = (height - 40) / 2
+        num = width // 100
+        temp = 0
+        icon = ""
+        for i in range(num):
+            temp = data['list'][i]['main']['temp']
+            icon = data['list'][i]['weather'][0]['icon']
+            time = data['list'][i]['dt_txt'][11:16]
+            timeLen = font24.getlength(time)
+            timeX = xOffset + (100 - timeLen) / 2
+            black_Channel.text((timeX, yOffset + y), time, font = font24, fill = 0)
+            rawTempStr = str(temp)
+            tempStr = f"{rawTempStr.split('.')[0]}°"
+            tempLen = font24.getlength(tempStr)
+            tempX = xOffset + (100 - tempLen) / 2
+            black_Channel.text((tempX, yOffset + y + 30), tempStr, font = font24, fill = 0)
+            if height >= 140:
+                if os.path.isfile(os.path.join(weatherdir, icon + ".bmp")):
+                    iconImg = Image.open(os.path.join(weatherdir, icon + ".bmp")).convert('L')
+                    iconImg = PIL.ImageOps.invert(iconImg)
+                    black_Channel.bitmap((xOffset + x, yOffset + y + 40), iconImg, fill = 0)
+                if os.path.isfile(os.path.join(weatherdir, icon + "r.bmp")):
+                    iconImg = Image.open(os.path.join(weatherdir, icon + "r.bmp")).convert('L')
+                    iconImg = PIL.ImageOps.invert(iconImg)
+                    red_Channel.bitmap((xOffset + x, yOffset + y + 40), iconImg, fill = 0)
+            xOffset += 100
+          
     except IOError as e:
         logging.info(e)
         
