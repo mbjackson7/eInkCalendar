e6a3288f2ef448e4e7cac2ee524201fa1b2cb619
Morgan Jackson <mbj4school@gmail.com>
2023-03-27 20:28:01 -0500

    blank calendar widget

---
+++ scripts/calendar.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import sys
+import os
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+fontdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic/fonts')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+widgetdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'widgets')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd7in5b_V2
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+from datetime import datetime
+import requests
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+logging.basicConfig(level=logging.DEBUG)
+
+def get_weather(width: int, height: int, x: int, y: int, red_Channel: ImageDraw, black_Channel: ImageDraw, font24, font18):
+    try:   
+        # TODO - Get google calendar events
+        pass
+    
+    except IOError as e:
+        logging.info(e)
+        
+    except KeyboardInterrupt:    
+        logging.info("ctrl + c:")
+        epd7in5b_V2.epdconfig.module_exit()
+        exit()
